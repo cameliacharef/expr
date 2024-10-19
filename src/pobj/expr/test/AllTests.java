e9df3d658ed17917cbb6f19534164af8425db23d
Antoine Miné <antoine.mine@lip6.fr>
2024-10-19 10:31:15 +0200

    Initial commit

---
+++ src/pobj/expr/test/AllTests.java
@@ -0,0 +1,10 @@
+package pobj.expr.test;
+
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectPackages("pobj.expr.test")
+public class AllTests {
+
+}
