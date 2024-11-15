b213c43767f21b17422954582c12eadd0aeeda8c
camelia <camelia@debian>
2024-11-15 18:02:41 +0100

    q10

---
+++ src/pobj/expr/Question10.java
@@ -0,0 +1,18 @@
+package pobj.expr;
+
+public class Question10 {
+	// retourne vrai si e est un arbre d’expression constant 
+	public static boolean isConstant(Expression e) {
+		VisitorConstant v = new VisitorConstant();
+		return e.accept(v); 
+	} 
+	// retourne la valeur d’une expression constante 
+	// signale une exception si l’expression n’est pas constante 
+	public static int evalConstantExpression (Expression e) {
+		if(!isConstant(e)) {
+			throw new UnsupportedOperationException();
+		}
+		VisitorEval v = new VisitorEval();
+		return e.accept(v);
+	}
+}
